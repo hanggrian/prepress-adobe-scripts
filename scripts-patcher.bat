e92edb8953d591b5ddcb7842ab731ddf424e8993
Hendra Anggrian <hendraanggrian@gmail.com>
2020-09-04 22:00:41 +0700

    Working Windows executable

--- scripts-patcher.bat
+++ scripts-patcher.bat
@@ -1,11 +1,62 @@
 @rem Windows executable
 @rem Copying `Scripts` directory to all Adobe Illustrator installation paths.
 
-@rem @echo off
+@echo off
+
+setlocal EnableDelayedExpansion
 
 set SOURCE=%~dp0Scripts
 
-for /d %%a in ("%PROGRAMFILES%"/*) do (
-    set APP_NAME=%%~nxa
-    echo %APP_NAME%
+echo Searching for Adobe Illustrator installation paths...
+echo.
+set "isEmpty=y"
+for /d %%a in ("%ProgramFiles%"\Adobe\*) do (
+    set appName=%%~nxa
+    if NOT !appName!==!appName:Illustrator=! (
+        for /d %%p in ("%%a"\Presets\*) do (
+            echo Patching to '%%p'...
+            set "isEmpty="
+            set scripts="%%p"\Scripts
+            if exist !scripts! (
+                echo Deleting existing scripts...
+                rmdir /s /q !scripts!
+            )
+            echo Copying new scripts...
+            robocopy /s /unicode "%SOURCE%" !scripts!
+            echo Finished.
+            echo.
+        )
+    )
+)
+if defined isEmpty (
+    echo Not found.
+    echo.
+)
+
+if exist "%ProgramFiles(x86)%" (
+    echo Searching for 32-bit Adobe Illustrator installation paths...
+    echo.
+    set "isEmpty=y"
+    for /d %%a in ("%ProgramFiles%"\Adobe\*) do (
+        set appName=%%~nxa
+        if NOT !appName!==!appName:Illustrator=! (
+            for /d %%p in ("%%a"\Presets\*) do (
+                echo Patching to '%%p'...
+                set "isEmpty="
+                set scripts="%%p"\Scripts
+                if exist !scripts! (
+                    echo Deleting existing scripts...
+                    rmdir /s /q !scripts!
+                )
+                echo Copying new scripts...
+                robocopy /s /unicode "%SOURCE%" !scripts!
+                echo Finished.
+                echo.
+            )
+        )
+    )
+    if defined isEmpty (
+        echo Not found.
+        echo.
+    )
 )
--- scripts-patcher.sh
+++ scripts-patcher.sh
@@ -3,18 +3,34 @@
 # Mac executable
 # Copying `Scripts` directory to all Adobe Illustrator installation paths.
 
+if ! [ $(uname) == "Darwin" ]; then
+    echo Unsupported platform.
+    exit 1
+fi
+
 SOURCE="$(cd `dirname $0` && pwd)/Scripts"
 
+echo -e "Searching for Adobe Illustrator installation paths...\n"
+isEmpty=true
 for app in /Applications/* ; do
     appName=`basename $app`
     if [[ $appName == *Adobe* ]] && [[ $appName == *Illustrator* ]] ; then
-        echo "Patching to '$app':"
         for preset in "$app"/Presets.localized/* ; do
-            echo "- $preset"
-            # remove whole folder
-            rm -rf "$preset"/Scripts
-            # bulk copy folder
+            echo Patching to \'$app\'...
+            isEmpty=false
+            scripts="$preset"/Scripts
+            if [ -d "$scripts" ] ; then
+                echo Deleting existing scripts...
+                rm -rf "$scripts"
+            fi
+            echo Copying new scripts...
             cp -r "$SOURCE" "$preset"
+            echo -e "Finished.\n"
         done
     fi
 done
+if [ $isEmpty = true ] ; then
+    echo -e "Not found.\n"
+fi
+
+exit 0
