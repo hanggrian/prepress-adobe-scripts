6bce5339564add2367830cee76069e7c8e667596
Hendra Anggrian <hendraanggrian@gmail.com>
2021-08-24 19:47:21 +0700

    `Types/Create Compound Path` supports multiple texts

--- Illustrator Scripts/Types/Create Compound Path.jsx
+++ Illustrator Scripts/Types/Create Compound Path.jsx
@@ -6,14 +6,18 @@ checkHasSelection()
 var items = selection.filterItem(function(it) { return it.typename === 'TextFrame' })
 check(items.isNotEmpty(), 'No types found in selection')
 
+var createdItems = []
 items.forEach(function(it) {
+    selection = [it] // isolate selection so that compound path are created individually
     var group = it.createOutline()
     for (var i = 0; i < group.compoundPathItems.length; i++) {
         var compoundPathItem = group.compoundPathItems[i]
         for (var j = 0; j < compoundPathItem.pathItems.length; j++) {
-            compoundPathItem.pathItems[j].duplicate(document, ElementPlacement.PLACEATBEGINNING)
+            compoundPathItem.pathItems[j].duplicate(document, ElementPlacement.PLACEATEND)
         }
     }
     group.remove()
     app.executeMenuCommand('compoundPath')
+    createdItems.push(selection.first())
 })
+selection = createdItems
